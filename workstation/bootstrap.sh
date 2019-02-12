5d410286c65e5f684ade44addbaaaae6ae6fbf74
Geries Simon <geries.simon@gmail.com>
2019-02-12 10:31:33 -0600

    update docker service to publish port

--- workstation/bootstrap.sh
+++ workstation/bootstrap.sh
@@ -43,7 +43,7 @@ TimeoutStartSec=0
 ExecStartPre=-/usr/bin/docker kill dev
 ExecStartPre=-/usr/bin/docker rm dev
 ExecStartPre=/usr/bin/docker pull geries/dev:latest
-ExecStart=/usr/bin/docker run -h dev -e TZ=America/Chicago --net=host --rm -v /var/run/docker.sock:/var/run/docker.sock -v /mnt/code:/root/code -v /mnt/secrets:/root/secrets --cap-add=SYS_PTRACE --security-opt seccomp=unconfined --privileged --name dev geries/dev:latest
+ExecStart=/usr/bin/docker run -h dev -e TZ=America/Chicago -p 1313:1313 --net=host --rm -v /var/run/docker.sock:/var/run/docker.sock -v /mnt/code:/root/code -v /mnt/secrets:/root/secrets --cap-add=SYS_PTRACE --security-opt seccomp=unconfined --privileged --name dev geries/dev:latest
 
 [Install]
 WantedBy=multi-user.target
