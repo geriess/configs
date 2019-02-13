7d2b73a46f7ff1e32d4ff0ca1bad116d5e4365e0
Geries Simon <geries.simon@gmail.com>
2019-02-12 22:50:46 -0600

    make port update and secret

--- workstation/pull-secrets.sh
+++ workstation/pull-secrets.sh
@@ -11,13 +11,17 @@ echo "Pulling secrets"
 op get document 'github_rsa' > github_rsa
 # op get document 'zsh_private' > zsh_private
 op get document 'zsh_history' > zsh_history
+op get document 'dokey' > dokey
 
 rm -f ~/.ssh/github_rsa
+rm -f ~/.ssh/dokey
 # rm -f ~/.zsh_private
 rm -f ~/.zsh_history
 
 ln -s $(pwd)/github_rsa ~/.ssh/github_rsa
 chmod 0600 ~/.ssh/github_rsa
+ln -s $(pwd)/dokey ~/.ssh/dokey
+chmod 0600 ~/.ssh/dokey
 # ln -s $(pwd)/zsh_private ~/.zsh_private
 ln -s $(pwd)/zsh_history ~/.zsh_history
 
